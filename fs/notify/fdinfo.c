5945b074c96857c06684fa96fb6ff2dfaeffdae2
CumaRull <102359107+cumaRull@users.noreply.github.com>
2025-10-18 12:40:06 +0700

    fs: fix mark as mask

--- fs/notify/fdinfo.c
+++ fs/notify/fdinfo.c
@@ -119,8 +119,7 @@ static void inotify_fdinfo(struct seq_file *m, struct fsnotify_mark *mark)
 				goto out_free_pathname;
 			}
 			seq_printf(m, "inotify wd:%x ino:%lx sdev:%x mask:%x ignored_mask:0 ",
-			   inode_mark->wd, path.dentry->d_inode->i_ino, path.dentry->d_inode->i_sb->s_dev,
-			   inotify_mark_user_mask(mark));
+			   inode_mark->wd, path.dentry->d_inode->i_ino, path.dentry->d_inode->i_sb->s_dev);
 			show_mark_fhandle(m, path.dentry->d_inode);
 			seq_putc(m, '\n');
 			iput(inode);
@@ -132,7 +131,7 @@ out_free_pathname:
 		}
 out_seq_printf:
 #endif
-		seq_printf(m, "inotify wd:%x ino:%lx sdev:%x mask:%x ignored_mask:%x ",
+		 seq_printf(m, "inotify wd:%x ino:%lx sdev:%x mask:%x ignored_mask:%x ",
 			   inode_mark->wd, inode->i_ino, inode->i_sb->s_dev,
 			   mask, mark->ignored_mask);
 		show_mark_fhandle(m, inode);
