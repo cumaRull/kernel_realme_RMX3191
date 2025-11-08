9f1722925234a0d732321bb9cbfb3b103c8025f6
CumaRull <102359107+cumaRull@users.noreply.github.com>
2025-11-08 14:05:24 +0000

    Revert "KernelSU: Implement scope-minimized manual hooks v1.4"
    This reverts commit be8b60c49b8e3bbe34661503add3bf2bd5876266.

--- fs/exec.c
+++ fs/exec.c
@@ -1720,14 +1720,6 @@ extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
 		void *argv, void *envp, int *flags);
 #endif /* CONFIG_KSU */
 
-#ifdef CONFIG_KSU
-extern bool ksu_execveat_hook __read_mostly;
-extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
-			void *envp, int *flags);
-extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
-				 void *argv, void *envp, int *flags);
-#endif
-
 /*
  * sys_execve() executes a new program.
  */
@@ -1747,7 +1739,7 @@ static int do_execveat_common(int fd, struct filename *filename,
 		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
 	else
 		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
-#endif
+#endif /* CONFIG_KSU */
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
--- fs/stat.c
+++ fs/stat.c
@@ -168,8 +168,7 @@ EXPORT_SYMBOL(vfs_statx_fd);
 
 #ifdef CONFIG_KSU
 extern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);
-
-#endif
+#endif /* CONFIG_KSU */
 
 /**
  * vfs_statx - Get basic and extra attributes by filename
@@ -195,8 +194,7 @@ int vfs_statx(int dfd, const char __user *filename, int flags,
 
 #ifdef CONFIG_KSU
 	ksu_handle_stat(&dfd, &filename, &flags);
-
-#endif
+#endif /* CONFIG_KSU */
 
 	if ((flags & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT |
 		       AT_EMPTY_PATH | KSTAT_QUERY_FLAGS)) != 0)
